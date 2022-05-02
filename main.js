9a7948ff1ffbf360c99a9ae9b6c0ffd3e8e1d5a2
Phillip Rak <rak.phillip@gmail.com>
2022-05-02 16:27:37 -0700

    Add a child page
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 const { app, BrowserWindow } = require('electron')
 
-const createWindow = () => {
+const createWindow = async () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600
   })
 
-  win.loadFile('index.html')
+  await win.loadFile('index.html')
+
+  return win;
 }
 
-app.whenReady().then(() => {
-  createWindow()
+const createChild = async (parent) => {
+  const win = new BrowserWindow({
+    parent,
+    width: 800,
+    height: 600,
+    modal: true,
+    useContentSize: true,
+    webPreferences: {
+      enablePreferredSizeMode: true,
+    },
+  });
+
+  win.webContents.on('preferred-size-changed', (_event, { width, height }) => {
+    win.setContentSize(width, height);
+  });
+
+  await win.loadFile('child.html')
+}
+
+app.whenReady().then(async () => {
+  const parent = await createWindow()
+  createChild(parent);
 })
